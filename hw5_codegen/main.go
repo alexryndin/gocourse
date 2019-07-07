669df35a5da79f5cf72fc939d0fe896bcfc223c5
Alex Ryndin <alexnryndin@gmail.com>
2019-07-07 22:59:04 +0300

    hw5 test passes

--- hw5_codegen/main.go
+++ hw5_codegen/main.go
@@ -9,85 +9,139 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
+	//	"net/url"
 	//	"reflect"
 	"strconv"
 )
 
-func marshalAndWrite(w http.ResponseWriter, resp map[string]interface{}) {
+func handleError(w http.ResponseWriter, err error) {
+	resp := make(map[string]interface{})
+	resp["error"] = ""
+	status := 500
+
+	switch errt := err.(type) {
+	case ApiError:
+		resp["error"] = errt.Error()
+		status = errt.HTTPStatus
+	default:
+		resp["error"] = errt.Error()
+	}
+	marshalAndWrite(w, resp, status)
+}
+
+func marshalAndWrite(w http.ResponseWriter, resp map[string]interface{}, status int) {
 	if enc, err := json.Marshal(resp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "InternalServerError")
 		return
 	} else {
-		fmt.Fprint(w, enc)
+		w.WriteHeader(status)
+		w.Write(enc)
 		return
 	}
 }
 
 func (h *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "URL:", r.URL.String())
-	fmt.Fprintln(w, "URL Path :", r.URL.Path)
 	switch r.URL.Path {
-	case "/user/Profile":
-		var resp map[string]interface{}
-		pp := ProfileParams{}
-		if err := pp.decode(r.URL.Query()); err != nil {
-			resp["error"] = err.Error()
-			marshalAndWrite(w, resp)
-			return
-		}
-		if err := pp.validate(); err != nil {
-			resp["error"] = err.Error()
-			marshalAndWrite(w, resp)
-		}
-		if profile, err := h.Profile(r.Context(), pp); err != nil {
-			resp["error"] = err.Error()
-			if enc, err := json.Marshal(resp); err != nil {
-				fmt.Fprint(w, "InternalServerError")
-				return
-			} else {
-				fmt.Fprint(w, enc)
-				return
-			}
-
-		}
-	case "/user/Create":
-		cp := CreateParams{}
-		if err := cp.decode(r.URL.Query()); err != nil {
-			fmt.Fprint(w, err.Error())
-			return
-		}
-
-		if err := cp.validate(); err != nil {
-			fmt.Fprint(w, err.Error())
-			return
-		}
-		h.Create(r.Context(), cp)
+	case "/user/profile":
+		h.profileWrapper(w, r)
+	case "/user/create":
+		h.createWrapper(w, r)
 	default:
-		fmt.Fprint(w, "404")
+		err := ApiError{
+			404,
+			fmt.Errorf("unknown method"),
+		}
+		handleError(w, err)
 		//	 h.wrapperDoSomeJob(w, r)
 	}
 }
 
-func (dst *ProfileParams) decode(src url.Values) error {
-	dst.Login = src.Get("login")
+func (h *MyApi) profileWrapper(w http.ResponseWriter, r *http.Request) {
+	pp := ProfileParams{}
+	if err := pp.decode(r); err != nil {
+		handleError(w, err)
+		return
+	}
+	if err := pp.validate(); err != nil {
+		handleError(w, err)
+		return
+	}
+	profile, err := h.Profile(r.Context(), pp)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	resp := make(map[string]interface{})
+	resp["response"] = profile
+	resp["error"] = ""
+	marshalAndWrite(w, resp, 200)
+}
+
+func (dst *ProfileParams) decode(r *http.Request) error {
+	dst.Login = r.FormValue("login")
+	fmt.Println(dst.Login)
 	return nil
 }
 
 func (dst *ProfileParams) validate() error {
 	if dst.Login == "" {
-		return fmt.Errorf("error: Login required")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("login must me not empty"),
+		}
 	}
 	return nil
 }
 
-func (dst *CreateParams) decode(src url.Values) error {
-	dst.Login = src.Get("login")
-	dst.Name = src.Get("full_name")
-	dst.Status = src.Get("status")
-	i, err := strconv.Atoi(src.Get("age"))
+func (h *MyApi) createWrapper(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		err := ApiError{
+			406,
+			fmt.Errorf("bad method"),
+		}
+		handleError(w, err)
+		return
+	}
+	if r.Header.Get("X-Auth") != "100500" {
+		err := ApiError{
+			403,
+			fmt.Errorf("unauthorized"),
+		}
+		handleError(w, err)
+		return
+	}
+
+	cp := CreateParams{}
+	if err := cp.decode(r); err != nil {
+		handleError(w, err)
+		return
+	}
+	if err := cp.validate(); err != nil {
+		handleError(w, err)
+		return
+	}
+	newuser, err := h.Create(r.Context(), cp)
 	if err != nil {
-		return fmt.Errorf("Couldn't parse str to int")
+		handleError(w, err)
+		return
+	}
+	resp := make(map[string]interface{})
+	resp["response"] = newuser
+	resp["error"] = ""
+	marshalAndWrite(w, resp, 200)
+}
+
+func (dst *CreateParams) decode(r *http.Request) error {
+	dst.Login = r.FormValue("login")
+	dst.Name = r.FormValue("full_name")
+	dst.Status = r.FormValue("status")
+	i, err := strconv.Atoi(r.FormValue("age"))
+	if err != nil {
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("age must be int"),
+		}
 	}
 	dst.Age = i
 	return nil
@@ -96,10 +150,16 @@ func (dst *CreateParams) decode(src url.Values) error {
 func (dst *CreateParams) validate() error {
 
 	if dst.Login == "" {
-		return fmt.Errorf("error: Login required")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("login must me not empty"),
+		}
 	}
 	if len(dst.Login) < 10 {
-		return fmt.Errorf("error:  Login must be > 10")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("login len must be >= 10"),
+		}
 	}
 	status_map := map[string]bool{
 		"user":      true,
@@ -110,13 +170,22 @@ func (dst *CreateParams) validate() error {
 		dst.Status = "user"
 	}
 	if _, present := status_map[dst.Status]; !present {
-		return fmt.Errorf("status must be one of [user, moderator, admin]")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("status must be one of [user, moderator, admin]"),
+		}
 	}
 	if dst.Age < 0 {
-		return fmt.Errorf("error:  age must be >= 0")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("age must be >= 0"),
+		}
 	}
 	if dst.Age > 128 {
-		return fmt.Errorf("error:  age must be < 128")
+		return ApiError{
+			http.StatusBadRequest,
+			fmt.Errorf("age must be <= 128"),
+		}
 	}
 	return nil
 }
